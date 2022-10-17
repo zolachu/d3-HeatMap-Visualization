722209f63e8475abb099678bdca5771fcd195733
Zola <zolboo@alumni.stanford.edu>
2022-10-17 16:47:34 -0700

    heatmap setup

---
+++ app.js
@@ -0,0 +1,56 @@
+async function draw(el, scale) {
+  // Data
+  const dataset = await d3.json("data.json");
+
+  // Dimensions
+  let dimensions = {
+    width: 600,
+    height: 150,
+  };
+
+  const box = 30;
+
+  // Draw Image
+  const svg = d3
+    .select(el)
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  // Scales
+  let colorScale;
+  if (scale === "linear") {
+    colorScale = d3
+      .scaleLinear()
+      .domain(d3.extent(dataset))
+      .range(["white", "red"]);
+  }
+
+  // Rectangles
+  svg
+    .append("g")
+    .attr("transform", "translate(2,2)")
+    .selectAll("rect")
+    .data(dataset)
+    .join((enter) => {
+      return enter
+        .append("rect")
+        .attr("width", box - 3)
+        .attr("height", box - 3)
+        .attr("fill", (d) => {
+          return colorScale(d);
+        })
+        .attr("x", (d, i) => {
+          console.log((i % 20) * box);
+          return (i % 20) * box;
+        })
+        .attr("y", (d, i) => {
+          return ((i / 20) | 0) * box;
+        })
+        .attr("stroke", "black");
+    });
+
+  console.log(svg.selectAll("circle"));
+}
+
+draw("#heatmap1", "linear");
